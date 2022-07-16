cbad43b0458b26ca658730a41c7a310c701c00e0
Ra1ski <ra1almaty@gmail.com>
2022-07-16 12:03:15 +0600

    Я реализовал роут О проекте

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-# @app.get("/about")
+@app.get("/about")
+def about():
+    return 'Это мой курсовой проект. Я делаю стартап на Python/FastAPI. Покажу рекрутеру.'
 
 # @app.get("/info")
 
